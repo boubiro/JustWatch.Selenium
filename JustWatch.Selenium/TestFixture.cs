1ace5fbe6b1cadd4447d8658805d2a03713c9033
olegbevz <olegdb@inbox.ru>
2017-07-29 08:30:04 +0300

    initial commit

---
+++ JustWatch.Selenium/TestFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JustWatch.Selenium
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [TestCase]
+        public void Test()
+        {
+            var driverService = FirefoxDriverService.CreateDefaultService();
+            driverService.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            driverService.HideCommandPromptWindow = true;
+            driverService.SuppressInitialDiagnosticInformation = true;
+            var driver = new FirefoxDriver(driverService, new FirefoxOptions(), TimeSpan.FromSeconds(60));
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("https://justwatches.ru");
+            
+            Assert.AreEqual(
+                "Justwatches — интернет-магазин наручных часов в Санкт-Петербурге", 
+                driver.Title);
+
+            //Close the browser
+            driver.Close();
+        }
+    }
+}
