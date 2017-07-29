7488ef8bd03a11c091d363618de684085d664761
olegbevz <olegdb@inbox.ru>
2017-07-29 18:27:03 +0300

    enabled product buy test.commented wait expressions.

--- JustWatch.Selenium/TestFixture.cs
+++ JustWatch.Selenium/TestFixture.cs
@@ -39,7 +39,7 @@ namespace JustWatch.Selenium
                 _driver.Title);            
         }
 
-        [TestCase, Ignore("not stable")]
+        [TestCase]
         public void ShouldBuyProduct()
         {
             // Open web site
@@ -49,27 +49,27 @@ namespace JustWatch.Selenium
             var manufacturersMenu = _driver.FindElement(By.CssSelector("nav#megamenu-menu ul.navbar-nav li.dropdown a[href=\"/brands/\"]"));
             var manufacturerImage = _driver.FindElement(By.CssSelector("li.megamenu-parent-block a.megamenu-parent-img img[title=\"Swiss Military\"]"));
             new Actions(_driver).MoveToElement(manufacturersMenu).Perform();
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("li.megamenu-parent-block a.megamenu-parent-img img[title=\"Swiss Military\"]")));
+            //WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("li.megamenu-parent-block a.megamenu-parent-img img[title=\"Swiss Military\"]")));
 
             // Click on Swiss Military image
             manufacturerImage.Click();
-            wait.Until(ExpectedConditions.StalenessOf(manufacturersMenu));
+            //wait.Until(ExpectedConditions.StalenessOf(manufacturersMenu));
 
             // Click on first product label
             var productLabel = _driver.FindElements(By.CssSelector("div.product>div.product-about>div.name>a")).FirstOrDefault();
             productLabel.Click();
-            wait.Until(ExpectedConditions.StalenessOf(productLabel));
+            //wait.Until(ExpectedConditions.StalenessOf(productLabel));
 
             // Click on cart button
             var button = _driver.FindElement(By.CssSelector("a#button-cart"));
             button.Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.mcartdiv")));
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.mcartdiv")));
 
             // Click on order button
             var orderButton = _driver.FindElements(By.CssSelector("div.mcartdiv a.testbutton")).FirstOrDefault();
             orderButton.Click();
-            wait.Until(ExpectedConditions.StalenessOf(orderButton));
+            //wait.Until(ExpectedConditions.StalenessOf(orderButton));
 
             // Populate payment form
             _driver.FindElement(By.CssSelector("input#input-payment-firstname")).SendKeys("Владимир");
