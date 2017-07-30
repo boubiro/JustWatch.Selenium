6d867086efe30317e9b39df2822a6e92985ab8d2
olegbevz <olegdb@inbox.ru>
2017-07-30 17:19:16 +0300

    _driver.FindElement(By.CssSelector("#button-cart")).Click();

--- JustWatch.Selenium/TestFixture.cs
+++ JustWatch.Selenium/TestFixture.cs
@@ -71,8 +71,8 @@ namespace JustWatch.Selenium
             _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("#button-cart")));
 
             // Click on cart button
-            _driver.ExecuteJavaScript("$('#button-cart').click();");
-            //_driver.FindElement(By.CssSelector("#button-cart")).Click();
+            //_driver.ExecuteJavaScript("$('#button-cart').click();");
+            _driver.FindElement(By.CssSelector("#button-cart")).Click();
             _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("a.testbutton")));
 
             // Click on order button
@@ -90,8 +90,8 @@ namespace JustWatch.Selenium
             _driver.FindElement(By.CssSelector("input[name=\"agree\"]")).Click();
             _driver.FindElement(By.CssSelector("input[name=\"payment_agree\"]")).Click();
 
-            _driver.FindElement(By.CssSelector("input#button-go")).Click();
-            _wait.Until(ExpectedConditions.UrlContains("route=checkout/success"));
+            //_driver.FindElement(By.CssSelector("input#button-go")).Click();
+            //_wait.Until(ExpectedConditions.UrlContains("route=checkout/success"));
         }
 
         public void WaitForPageToLoad()
