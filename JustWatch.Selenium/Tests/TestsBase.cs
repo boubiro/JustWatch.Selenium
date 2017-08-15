8540adbd2f56680794107100e8867ab1c710c7fa
Oleg Bevz <obevz@luxoft.com>
2017-08-15 12:03:52 +0300

    integrated log4net to the project

--- JustWatch.Selenium/Tests/TestsBase.cs
+++ JustWatch.Selenium/Tests/TestsBase.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using log4net;
 
 namespace JustWatch.Selenium.Tests
 {
@@ -9,6 +10,12 @@ namespace JustWatch.Selenium.Tests
     {
         protected IWebDriver _driver;
         protected IWait<IWebDriver> _wait;
+        protected ILog _logger;
+
+        static TestsBase()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
 
         [OneTimeSetUp]
         public void Init()
@@ -16,18 +23,32 @@ namespace JustWatch.Selenium.Tests
             var driverFactory = new FirefoxDriverFactory();
             _driver = driverFactory.Create();
             _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            var type = GetType();
+            _logger = LogManager.GetLogger(GetType());
+
+            _logger.Debug("OneTimeSetup");
         }
 
         [OneTimeTearDown]
         public void CleanUp()
         {
             _driver.Quit();
+
+            _logger.Debug("OneTimeTearDown");
         }
 
         [SetUp]
         public void RunBeforeEachTest()
         {
             _driver.Navigate().GoToUrl(AppSettings.WebSiteUrl);
+
+            _logger.Debug("SetUp");
+        }
+
+        [TearDown]
+        public void RunAfterEachTest()
+        {
+            _logger.Debug("TearDown");
         }
     }
 }
